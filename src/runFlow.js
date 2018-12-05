1d325cdf411515039b2559ea61975b9a55b3b93b
Andrew Patton <andrew@acusti.ca>
2018-12-05 09:51:12 -0800

    Add support for an env variable that tells flow-watch to not clear the console on every run (#10)
    * Add env variable for overriding console clearing
    
    * Update README with mention of FLOW_WATCH_NO_CLEAR_CONSOLE

--- src/runFlow.js
+++ src/runFlow.js
@@ -8,12 +8,11 @@ try {
   flow = 'flow'
 }
 
-// Only clear the console if it's an interactive terminal.
-if (process.stdout.isTTY) {
+// Only clear the console if it's an interactive terminal & no env var override.
+if (process.stdout.isTTY && !process.env.FLOW_WATCH_NO_CLEAR_CONSOLE) {
   process.stdout.write('\u001b[2J')
   process.stdout.write('\u001b[1;1H')
   process.stdout.write('\u001b[3J')
 }
 
 require('cross-spawn')(flow, {stdio: 'inherit'})
-
